e4da389a22afc945e4b7d7c87b1d05116af856b5
Azuerm <3281186790@qq.com>
2025-03-23 21:28:16 +0800

    feat: axios封装

--- Ajax学习/项目/page/login/index.js
+++ Ajax学习/项目/page/login/index.js
@@ -5,3 +5,33 @@
  * 1.3 基于 axios 调用验证码登录接口
  * 1.4 使用 Bootstrap 的 Alert 警告框反馈结果给用户
  */
+
+document.querySelector('.btn').addEventListener('click',function() {
+    const form = document.querySelector('.login-form')
+    const data = serialize(form,{hash:true,empty:true})
+    axios({
+        // 因为有在request配置请求基地址
+        url:'/v1_0/authorizations',
+        method: 'post',
+        data: {
+            mobile: data.mobile,
+            code: data.code
+        }
+    }).then( result => {
+        console.log(result)
+        myAlert(true, '登录成功')
+
+        // 1.2 登录成功后，保存 token 令牌字符串到本地，并跳转到内容列表页面
+        localStorage.setItem('token',result.data.token)
+        setTimeout(() => {
+            // 延迟跳转，让alert警告框停留一会儿
+            location.href = '../content/index.html'
+        }, 2000)
+    }).catch( error => {
+        // 错误信息从response里面看
+        console.dir(error)
+        console.log(error.response.data.message)
+        myAlert(false, error.response.data.message)
+    })
+    
+})
---
+++ Node/01.js
@@ -0,0 +1,7 @@
+//  node是基于谷歌浏览器的v8引擎封装的，独立执行javascript代码的环境
+// 作用：编写后端程序，、前端工程化
+console.log('heolo')
+for( let i = 0; i < 3; i++) { 
+    console.log(6);
+    
+}
